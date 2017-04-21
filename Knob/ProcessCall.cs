06a3c8a7e966f958a0ce5f0d00b48094b3f2e12c
Adam Hafidz Fitrasani <adamhafidzf@gmail.com>
2017-04-21 11:18:39 +0900

    Adding basic tagging to ProcessClass
    Creating methods for accessing and modifying attributes

--- Knob/ProcessCall.cs
+++ Knob/ProcessCall.cs
@@ -20,6 +20,7 @@ namespace Knob
         private LinkedList<string> createFileLog;
         private LinkedList<string> writeFileLog;
         private LinkedList<string> deleteFileLog;
+        private bool alert;
 
         ProcessCall(string processName)
         {
@@ -32,15 +33,83 @@ namespace Knob
             iatSeverity = 0;
             antiDebugSeverity = 0;
             screencapSeverity = 0;
+            alert = false;
 
             createFileLog = new LinkedList<string>();
             writeFileLog = new LinkedList<string>();
             deleteFileLog = new LinkedList<string>();
         }
 
-        public void setFlag(string type)
+        public void setFlag(string type,int number)
         {
+            if (type.Equals("dll"))
+            {
+                if (dllInjection[number] == 0)
+                {
+                    dllInjection[number] = 1;
+                    dllSeverity++;
+                }
+            }
+            else if (type.Equals("iat"))
+            {
+                if (iatHooking[number] == 0)
+                {
+                    iatHooking[number] = 1;
+                    iatSeverity++;
+                }
+            }
+            else if (type.Equals("anti"))
+            {
+                if (antiDebugging[number] == 0)
+                {
+                    antiDebugging[number] = 1;
+                    antiDebugSeverity++;
+                }
+            }
+            else
+            {
+                if (screenCapture[number] == 0)
+                {
+                    screenCapture[number] = 1;
+                    screencapSeverity++;
+                }
+            }
+            checkSeverity();
+        }
 
+        private void checkSeverity()
+        {
+            if (iatSeverity == 2 || dllSeverity == 2 || screencapSeverity == 2)
+            {
+                alert = true;
+            }
+            else if (antiDebugSeverity >=1 )
+            {
+                alert = true;
+            }
+        }
+        public bool Alert
+        {
+            get
+            {
+                return Alert;
+            }
+        }
+
+        public void setLog(string call, string directory)
+        {
+            if (call.Equals("CreateFile"))
+            {
+                createFileLog.AddLast(directory);
+            }
+            else if (call.Equals("DeleteFile"))
+            {
+                deleteFileLog.AddLast(directory);
+            }
+            else
+            {
+                writeFileLog.AddLast(directory);
+            }
         }
     }
 }
